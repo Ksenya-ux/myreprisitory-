84ccc5be964911ee5fbdf14df1c941005d794ac8
Kseniyaikovskaya <krikovskaya06@gmail.com>
2025-09-29 09:22:37 +0300

    class work

---
+++ 20250929/0/task1.py
@@ -0,0 +1,7 @@
+l = list(eval(input()))
+for i in l:
+	if  i == '13':
+		print('13 is here')
+		break
+else:
+	print('there are not 13')
---
+++ 20250929/0/task2.py
@@ -0,0 +1,4 @@
+num = [i for i in range(5,16)]
+alp = ['a','b','c','d','e','f','g','h','i','j','k']
+num[4:7] = alp[-5:]
+print(num)
