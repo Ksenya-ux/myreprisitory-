63475b983b53fa428378146a4bb124627ee40fa3
Kseniyaikovskaya <krikovskaya06@gmail.com>
2025-11-17 10:08:10 +0300

    cw

---
+++ 20251117/0/task.py
@@ -0,0 +1,37 @@
+class Counter:
+    def __init__(self):
+        self.count = 0
+    
+    def __get__(self, instance, owner):
+        return self.count
+    
+    def __set__(self, instance, value):
+        pass
+    
+    def __iadd__(self, other):
+        self.count += other
+        return self
+    
+    def __isub__(self, other):
+        self.count -= other
+        return self
+
+
+class C:
+    counter = Counter()
+    
+    def __init__(self):
+        self.counter += 1
+    
+    def __del__(self):
+        self.counter -= 1
+
+
+c = C()
+print(f"c.counter = {c.counter}")
+
+d = C()
+print(f"d.counter = {d.counter}")
+
+del c
+print(f"d.counter = {d.counter}")
