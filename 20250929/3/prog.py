93a257b6a82937b5b9db4a3b88fb699cf2b772c3
Kseniyaikovskaya <krikovskaya06@gmail.com>
2025-12-23 05:33:24 +0300

    -m

--- 20250929/3/prog.py
+++ 20250929/3/prog.py
@@ -1,22 +1,37 @@
-s = input()
-m = []
-ans = []
-while s != '':
-    s = s.split(',')
-    s = [int(i) for i in s]
-    m.append(s)
-    s = input()
-n = len(m) // 2
-m1 = m[:n]
-m2 = m[n:]
-ans = []
-for i in range(n):
-    temp = []
-    for j in range(n):
-        st = 0
-        for k in range(n):
-            st += m1[i][k] * m2[k][j]
-        temp.append(st)
-    ans.append(temp)
-for i in ans:
-    print(*i)
+import sys
+
+data = []
+try:
+    while True:
+        text = input()
+        if text == "":
+            break
+        data.append(text.strip())
+except EOFError:
+    pass
+
+if not data:
+    import sys
+    data = [line.strip() for line in sys.stdin if line.strip()]
+
+size = len(data) // 2
+
+first_matrix = []
+second_matrix = []
+
+for idx in range(size):
+    first_matrix.append(list(map(int, data[idx].split(','))))
+    second_matrix.append(list(map(int, data[idx + size].split(','))))
+
+output = []
+for i in range(size):
+    current_row = []
+    for j in range(size):
+        total = 0
+        for k in range(size):
+            total += first_matrix[i][k] * second_matrix[k][j]
+        current_row.append(str(total))
+    output.append(','.join(current_row))
+
+for row in output:
+    print(row)
