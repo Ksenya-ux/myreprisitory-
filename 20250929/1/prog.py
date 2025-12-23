ec5bd55eaa4842e7f4edec662518ea67081860d0
Kseniyaikovskaya <krikovskaya06@gmail.com>
2025-12-23 07:08:13 +0300

    QQQQ

--- 20250929/1/prog.py
+++ 20250929/1/prog.py
@@ -1,2 +1,2 @@
-m, n = eval(input())
-print([x for x in range(m,n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+start, end = map(int, input().split(','))
+print([num for num in range(start, end) if all(num % divisor != 0 for divisor in range(2, int(num**0.5) + 1)) and num > 1] if start <= end else [])
